9125486ac1eece55421039d94d27f8563e00decf
YCaTGameRT <ycatgamert@gmail.com>
2026-04-25 07:12:34 +0500

    добавленна поддержка русского языка

--- SeniorShop/main.cpp
+++ SeniorShop/main.cpp
@@ -1,6 +1,11 @@
+#include <Windows.h>
+
 #include "LoginSystem.h"
 
 int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
     LoginSystem login_system;
     login_system.launch();
     return 0;
