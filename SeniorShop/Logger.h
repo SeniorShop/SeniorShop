27f099ebb463db3bfd1f4befa7ec9a6806a8c2d0
fr1zb3e <168219806+Frolotey1@users.noreply.github.com>
2026-04-29 00:40:36 +0500

    Delete SeniorShop directory

--- SeniorShop/Logger.h
+++
@@ -1,9 +0,0 @@
-#ifndef LOGGER_H
-#define LOGGER_H
-#include "Libraries.h"
-
-class Logger {
-public:
-    static void log_attempt(const std::string& username, bool success);
-};
-#endif // !LOGGER_HPP
