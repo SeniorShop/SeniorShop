95a863d8313a4b76e4ef179fe03512b7f408a2c3
YCaTGameRT <ycatgamert@gmail.com>
2026-05-01 21:41:48 +0500

    writed h and created cpp

---
+++ Personal.cpp
@@ -0,0 +1,23 @@
+#include "Personal.h"
+
+int Personal::ManagePersonal() {
+
+}
+bool Personal::IsSupAdmin() {
+
+}
+void Personal::AddUser() {
+
+}
+void Personal::DeleteUser() {
+
+}
+void Personal::UserPassChange() {
+
+}
+void Personal::UserRoleChange() {
+
+}
+void Personal::PrintUsers() {
+
+}
--- Personal.h
+++ Personal.h
@@ -1,5 +1,22 @@
 #ifndef _PERSONAL_H_
 #define _PERSONAL_H_
 
+#include "Libraries.h"
+#include "User.h"
+
+class Personal {
+public:
+	int ManagePersonal();
+	bool IsSupAdmin();
+	void AddUser();
+	void DeleteUser();
+	void UserPassChange();
+	void UserRoleChange();
+	void PrintUsers();
+protected:
+
+private:
+
+};
 
 #endif
